bb562190d9680d2b96a2b4ff5a28be2c14ef4dbe
changye <changye@thinkerx.com>
2017-09-06 17:17:33 +0800

    modify pager.vue,use el-pagination in elt-pagination

--- packages/elt-pagination/src/pager.vue
+++ packages/elt-pagination/src/pager.vue
@@ -1,32 +1,15 @@
 <template>
-  <ul @click="onPagerClick" class="el-pager">
-    <li
-      :class="{ active: currentPage === 1 }"
-      v-if="pageCount > 0"
-      class="number">1</li>
-    <li
-      class="el-icon more btn-quickprev"
-      :class="[quickprevIconClass]"
-      v-if="showPrevMore"
-      @mouseenter="quickprevIconClass = 'el-icon-d-arrow-left'"
-      @mouseleave="quickprevIconClass = 'el-icon-more'">
-    </li>
-    <li
-      v-for="pager in pagers"
-      :class="{ active: currentPage === pager }"
-      class="number">{{ pager }}</li>
-    <li
-      class="el-icon more btn-quicknext"
-      :class="[quicknextIconClass]"
-      v-if="showNextMore"
-      @mouseenter="quicknextIconClass = 'el-icon-d-arrow-right'"
-      @mouseleave="quicknextIconClass = 'el-icon-more'">
-    </li>
-    <li
-      :class="{ active: currentPage === pageCount }"
-      class="number"
-      v-if="pageCount > 1">{{ pageCount }}</li>
-  </ul>
+  <div>
+    <el-pagination
+      :small="small"
+      layout="layout"
+      :total="total"
+      :page-size="pageSize"
+      :page-sizes="pageSizes"
+      :page-count="pageCount"
+      :current-page="currentPage">
+    </el-pagination>
+  </div>
 </template>
 
 <script type="text/babel">
