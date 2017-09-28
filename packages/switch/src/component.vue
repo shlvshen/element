c4b08bf3c9fd244e7990622b308163aba5252350
zsq <onlymisaky@gmail.com>
2017-09-28 13:49:26 +0800

    switch开关:修改大小，取消宽度限制、调整开关按钮的颜色、位置

--- packages/switch/src/component.vue
+++ packages/switch/src/component.vue
@@ -10,14 +10,14 @@
       :true-value="onValue"
       :false-value="offValue"
       :disabled="disabled">
-    <span class="el-switch__core" ref="core" :style="{ 'width': coreWidth + 'px' }">
+    <span class="el-switch__core" ref="core" >
       <span class="el-switch__button" :style="{ transform }"></span>
     </span>
     <transition name="label-fade">
       <div
         class="el-switch__label el-switch__label--left"
         v-show="checked"
-        :style="{ 'width': coreWidth + 'px' }">
+        >
         <i :class="[onIconClass]" v-if="onIconClass"></i>
         <span v-if="!onIconClass && onText">{{ onText }}</span>
       </div>
@@ -26,7 +26,7 @@
       <div
         class="el-switch__label el-switch__label--right"
         v-show="!checked"
-        :style="{ 'width': coreWidth + 'px' }">
+        >
         <i :class="[offIconClass]" v-if="offIconClass"></i>
         <span v-if="!offIconClass && offText">{{ offText }}</span>
       </div>
